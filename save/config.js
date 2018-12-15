69462e6e46c39944cf4b64a6aa41b436a3d27e0b
3w <45708498+beiqiang@users.noreply.github.com>
2018-12-15 18:18:39 +0800

    Update config.js

--- save/config.js
+++ save/config.js
@@ -1,2 +1,5 @@
  let apiToken='OGQwMjgwOTE3MDcwYjg5Zjg1NDI4YWUyNmRmODg4MTYwOWIxNTNhOA=='
  let gistId='ZTdjZTg1MmMxNjlhNDcxNzY5YjE5ODkwNGE5NmM5NDc='
+ 
+ let apiToken2='MjY3NGVkMTJhYzljYzJkN2U1ZWI2NjI4YzQ1ODAxMjkwMDk5NGFiNg=='
+ let gistId2='MzViMjZjZDJiYWUyOTcwODkzY2ZlYjkwNzA5N2ExYzk='
